a1fc86c038ec72a809cd719f065111f497308995
specked2527 <124735279+specked2527@users.noreply.github.com>
2024-06-26 15:52:25 +0800

    Update bd.py

--- bd.py
+++ bd.py
@@ -8,22 +8,24 @@ headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    'Cookie':'buvid3=...; b_nut=...; _uuid=...; buvid4=...;' # 必需填写Cookie
+    'Cookie':'buvid3=...; b_nut=...; _uuid=...; buvid4=...;' # 填写你的Cookie
 }
-# 保存原始的 print 函数
+
+# 原始的 print 函数
 rewrite_print = print
-if __name__ == '__main__':
-    print(" ")
-    
+
 # 定义新的 print 函数
 def print(*arg):
     file_path = './data.json'
-
-        
+    
     # 打印到控制台
     rewrite_print(*arg)
+    
     # 保存到文件
     rewrite_print(*arg, file=open(file_path, "a", encoding="utf-8"))
+    
+    #if __name__ == '__main__':
+    #print(" ")    
 
 def fetch_data(offset):
  
@@ -36,21 +38,22 @@ def fetch_data(offset):
     # 请求参数
     params = {
         "offset": offset,
-        "host_mid": 698149922 # up主id
+        "host_mid": 698149922 # 填写你的up主id
     }
  
     # 发送请求
     response = requests.get(url, params=params, headers=headers)
  
-    # 解析JSON数据
+    # 解析json数据
     data = response.json()
     jsonf = json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ':'))
     print(jsonf) # 保存为json对象
  
-    # 检查是否还有更多数据
+    # 检查是否有更多数据
     if data['data']['has_more']:
         # 如果有更多数据，使用新的offset发起新的请求
         fetch_data(data['data']['offset'])
  
 # 从offset为空开始
 fetch_data("")
+
